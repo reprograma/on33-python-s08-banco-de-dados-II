8aaa82c506e82c52059c993cfd702c6806a45ea2
Aline Yoda <alineyoda@live.com>
2024-07-25 21:34:58 -0300

    "Exercícios resolvidos"

---
+++ exercicios/para-sala/resolucoes/ex2.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+conn = sqlite3.connect('escola.db')
+cursor = conn.cursor()
+
+estudantes = [
+       ('Alice', 21),
+       ('Bob', 22),
+       ('Charlie', 23)
+   ]
+
+cursor.executemany("INSERT INTO estudantes (nome, idade) VALUES (?, ?)", estudantes)
+
+conn.commit()
+cursor.close()
+conn.close()

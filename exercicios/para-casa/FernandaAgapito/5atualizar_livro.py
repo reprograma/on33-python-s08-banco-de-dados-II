217f8e02229408000547647885b7e60cb7b7f1ed
agapitoo <fernandaagapito2013@gmail.com>
2024-07-27 19:45:32 -0300

    AtividadeS08

--- exercicios/para-casa/FernandaAgapito/5atualizar_livro.py
+++ exercicios/para-casa/FernandaAgapito/5atualizar_livro.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('livraria.db')
 cursor = conn.cursor()
 
-cursor.execute("DELETE FROM livros WHERE titulo = ?", ('O filósofo, a enfermeira e o trapaceiro', ))
+cursor.execute("UPDATE livros SET preco = ? WHERE titulo = ?", (2.99, 'Solaris'))
 
 conn.commit()
 cursor.close()
--- exercicios/para-casa/FernandaAgapito/6remover_livro.py
+++ exercicios/para-casa/FernandaAgapito/6remover_livro.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('livraria.db')
 cursor = conn.cursor()
 
-cursor.execute("UPDATE livros SET preco = ? WHERE titulo = ?", (100, 'Introdução à Programação com Python'))
+cursor.execute("DELETE FROM livros WHERE titulo = ?", ('Solaris', ))
 
 conn.commit()
 cursor.close()
